63eec7814868c7af36efc7780e3ae9619b28d660
Kevin Lee <k.lee@nyu.edu>
2022-10-09 16:08:41 -0400

    Initialized Algo setup

---
+++ prediction/train.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import utils
+#import argparse
+
